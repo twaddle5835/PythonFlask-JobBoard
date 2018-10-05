7a54906f15fcdc8c57be0d3fe467d7cba9400da7
Tim Waddle <twaddle@daytonfreight.com>
2018-10-05 11:47:24 -0400

    Fixes

--- jobs/app.py
+++ jobs/app.py
@@ -22,7 +22,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone if single else cursor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
 
     cursor.close()
     return results
